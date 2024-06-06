3b5b0344ee39cf862c86ba1a7cd1b550c13a12e5
kevin olson <acidjazz@gmail.com>
2024-06-06 18:31:35 -0500

    :monocle_face: add deatils to purl entries that cves were found in

--- pkg/models/scan.go
+++ pkg/models/scan.go
@@ -4,12 +4,20 @@ type ScanResult struct {
 	Vulnerabilities []ScanResultVulnerabilities `json:"vulnerabilities"`
 }
 
+type PurlDetail struct {
+	Purl        string   `json:"purl"`
+	PackageType string   `json:"type"`
+	Cataloger   string   `json:"cataloger"`
+	Locations   []string `json:"locations"`
+}
+
 type ScanResultVulnerabilities struct {
-	Name              string `json:"name"`
-	Version           string `json:"version"`
-	CVE               string `json:"cve"`
-	InKEV             bool   `json:"in_kev"`
-	CVSSBaseScore     string `json:"cvss_base_score"`
-	CVSSTemporalScore string `json:"cvss_temporal_score"`
-	FixedVersions     string `json:"fixed_versions"`
+	Name              string     `json:"name"`
+	Version           string     `json:"version"`
+	CVE               string     `json:"cve"`
+	InKEV             bool       `json:"in_kev"`
+	CVSSBaseScore     string     `json:"cvss_base_score"`
+	CVSSTemporalScore string     `json:"cvss_temporal_score"`
+	FixedVersions     string     `json:"fixed_versions"`
+	PurlDetail        PurlDetail `json:"purl_detail"`
 }
