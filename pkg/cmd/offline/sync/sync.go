7937504676e34719d07ab0a97f7091d1817abb24
kevin olson <acidjazz@gmail.com>
2024-09-30 13:16:59 -0500

    🚧 choose mode is optional

--- pkg/cmd/offline/sync/sync.go
+++ pkg/cmd/offline/sync/sync.go
@@ -22,6 +22,7 @@ func Command() *cobra.Command {
 		Long:    "Sync indices for offline use",
 		Example: "vulncheck offline sync",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			choose, _ := cmd.Flags().GetBool("choose")
 
 			response, err := session.Connect(config.Token()).GetIndices()
 
@@ -35,29 +36,32 @@ func Command() *cobra.Command {
 				return err
 			}
 
-			options := make([]huh.Option[string], len(indices))
+			if len(selectedIndices) == 0 || choose {
 
-			for i, index := range indices {
-				options[i] = huh.Option[string]{
-					Value: index.Name,
-					Key:   index.Name,
+				options := make([]huh.Option[string], len(indices))
+
+				for i, index := range indices {
+					options[i] = huh.Option[string]{
+						Value: index.Name,
+						Key:   index.Name,
+					}
 				}
-			}
 
-			form := huh.NewForm(
-				huh.NewGroup(
-					huh.NewMultiSelect[string]().
-						Title("Select indices to sync").
-						Options(options...).
-						Height(10).
-						Filterable(true).
-						Value(&selectedIndices),
-				),
-			)
+				form := huh.NewForm(
+					huh.NewGroup(
+						huh.NewMultiSelect[string]().
+							Title("Select indices to sync").
+							Options(options...).
+							Height(10).
+							Filterable(true).
+							Value(&selectedIndices),
+					),
+				)
 
-			err = form.Run()
-			if err != nil {
-				return err
+				err = form.Run()
+				if err != nil {
+					return err
+				}
 			}
 
 			ui.Info(fmt.Sprintf("Syncing indices: %s", strings.Join(selectedIndices, ", ")))
@@ -69,6 +73,8 @@ func Command() *cobra.Command {
 			return nil
 		},
 	}
+
+	cmd.Flags().BoolP("choose", "c", false, "Prompt to choose indices to sync, even if cached ones exist")
 
 	return cmd
 }
