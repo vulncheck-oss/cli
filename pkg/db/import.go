0c5c9afe65ca3875b342571c45a12da62edb32e0
kevin olson <acidjazz@gmail.com>
2025-04-01 16:23:29 -0500

    🚧 update mod

--- pkg/db/import.go
+++ pkg/db/import.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"github.com/octoper/go-ray"
 	"github.com/vulncheck-oss/cli/pkg/cmd/offline/packages"
 	"os"
 	"path/filepath"
@@ -21,7 +20,6 @@ func ImportIndex(filePath string, indexDir string, progressCallback func(int)) e
 	// Get schema for this index type
 	indexName := packages.IndexFromName(filepath.Base(indexDir))
 	schema := GetSchema(indexName)
-	ray.Ray(indexName, schema)
 	if schema == nil {
 		return fmt.Errorf("no schema found for index %s", indexName)
 	}
